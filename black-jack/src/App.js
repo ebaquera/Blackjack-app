8481963b32dedb25235cc50f0e2f5c028abb17b6
ebaquera <edna.baquera@gmail.com>
2021-08-02 19:49:36 -0700

    ready to host

---
+++ black-jack/src/App.js
@@ -0,0 +1,17 @@
+import "./main/index.css";
+import Body from "./main/index.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>My first app</p>
+      </header>
+      <div>
+        <Body />
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- black-jack/src/main/index.jsx
+++ black-jack/src/main/index.jsx
@@ -1,24 +1,25 @@
-<html>
-    <head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
-        <link rel="stylesheet" href="index.css">
-    </head>
-    <body>
+import "./index.js"
+const Body = () =>{
+    return (
+        <div>
         <h1>Blackjack</h1>
         <p>Price per game: 5</p>
         <p id="dealer-card">Dealer Cards:</p>
         <p id='sum-deal'></p>
         <p id="message-el">Want to play a round?</p>
-        <button id='start-btn'></button>
+        <button id='start-btn'>START</button>
         <button id = 'new-card'>HIT</button>
         <button id = 'hold-btn'>STAND</button>
-        <br>
-        <p id = enter-name>Enter your name:</p>
-        <input type = 'text' id="player-name">
+        <br/>
+        <p id = 'enter-name'>Enter your name:</p>
+        <input type = 'text' id="player-name"/>
         <button id = 'add-name'>SUBMIT</button>
         <p id="cards-el">Your cards:</p>
         <p id="sum-el">Sum:</p>
         <p id="player-el"></p>
         <p id="chips-el"></p>
         <script src="index.js"></script>
-    </body>
-</html>
+        </div>
+        )
+    }
+    export default Body
